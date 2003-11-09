567bdd273c4ee42ce501be9427af301b8e122eef
gdshaw <gdshaw@40966042-f822-0410-a469-afe5762bee57>
2003-11-09 20:29:41 +0000

    Added class pkg::control_binary.

---
+++ !LibPkg/libpkg/control_binary.cc
@@ -0,0 +1,47 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#include "libpkg/control_binary.h"
+
+namespace pkg {
+
+static const map<string,int>& init_priorities()
+{
+	static map<string,int> priorities;
+	int pr=0;
+	priorities["md5sum"]=--pr;
+	priorities["size"]=--pr;
+	priorities["url"]=--pr;
+	priorities["conflicts"]=--pr;
+	priorities["suggests"]=--pr;
+	priorities["recommends"]=--pr;
+	priorities["depends"]=--pr;
+	priorities["version"]=--pr;
+	priorities["source"]=--pr;
+	priorities["architecture"]=--pr;
+	priorities["maintainer"]=--pr;
+	priorities["installed-size"]=--pr;
+	priorities["section"]=--pr;
+	priorities["priority"]=--pr;
+	priorities["package"]=--pr;
+	pr=0;
+	priorities["description"]=++pr;
+	return priorities;
+}
+
+control_binary::control_binary()
+{}
+
+control_binary::~control_binary()
+{}
+
+int control_binary::priority(const string& value) const
+{
+	static const map<string,int>& priorities=init_priorities();
+	map<string,int>::const_iterator f=priorities.find(value);
+	return (f!=priorities.end())?(*f).second:0;
+}
+
+}; /* namespace pkg */
