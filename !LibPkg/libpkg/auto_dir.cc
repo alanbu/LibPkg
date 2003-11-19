ff1c092f2cdf9ec6b48f0514866ea37841fc00cb
gdshaw <gdshaw@40966042-f822-0410-a469-afe5762bee57>
2003-11-19 20:16:07 +0000

    Added class pkg::auto_dir.

---
+++ !LibPkg/libpkg/auto_dir.cc
@@ -0,0 +1,88 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#include <algorithm>
+
+#include "rtk/os/os.h"
+
+#include "auto_dir.h"
+
+namespace pkg {
+
+namespace {
+
+/** Get length of prefix common to pathnames.
+ * The common prefix is composed of complete path components that occur
+ * in both pathnames.  Thus, the common prefix for the pathnames "a.b1.c"
+ * and "a.b2.c" is "a" (as opposed to "a.b").
+ * @param a the first pathname
+ * @param b the second pathname
+ * @return the length of the common prefix.
+ */
+unsigned int common(const string& a,const string& b)
+{
+	unsigned int i=0;
+	unsigned int e=min(a.length(),b.length());
+	while ((i<e)&&(a[i]==b[i])) ++i;
+	while ((i>0)&&(a[i-1]!='.')) --i;
+	if (i>0) --i;
+	return i;
+}
+
+}; /* anonymous namespace */
+
+auto_dir::auto_dir()
+{}
+
+auto_dir::~auto_dir()
+{}
+
+void auto_dir::operator()(const string& pathname)
+{
+	unsigned int i=_pathname.length();
+	unsigned int j=pathname.length();
+	unsigned int k=common(pathname,_pathname);
+
+	// Remove component from pathname.
+	// If common prefix not reached then deleted directory and repeat.
+	while (i>k)
+	{
+		--i;
+		while ((i>k)&&(_pathname[i]!='.')) --i;
+		if (i>k)
+		{
+			// There is no need to check whether the directory
+			// is empty, because an attempt to delete a non-empty
+			// directory should fail.
+			string dirname(_pathname,0,i);
+			try
+			{
+				rtk::os::OS_File6(dirname.c_str(),0,0,0,0,0);
+			}
+			catch (...) {}
+		}
+	}
+
+	// Add component to pathname.
+	// If requested pathname not reached then create directory and repeat.
+	while (i<j)
+	{
+		++i;
+		while ((i<j)&&(pathname[i]!='.')) ++i;
+		if (i<j)
+		{
+			string dirname(pathname,0,i);
+			try
+			{
+				rtk::os::OS_File8(dirname.c_str(),0);
+			}
+			catch (...) {}
+		}
+	}
+
+	_pathname=pathname;
+}
+
+}; /* namespace pkg */
---
+++ !LibPkg/libpkg/auto_dir.h
@@ -0,0 +1,44 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#ifndef _LIBPKG_AUTO_DIR
+#define _LIBPKG_AUTO_DIR
+
+#include <string>
+
+namespace pkg {
+
+/** A class for automatically creating and deleting directories.
+ * When an object of this class moves to a given pathname, it creates
+ * any directories needed to reach that pathname.  When it leaves a
+ * pathname, it deletes any directories that are no longer in use.
+ */
+class auto_dir
+{
+private:
+	/** The current pathname. */
+	string _pathname;
+public:
+	/** Construct auto-directory object.
+	 * The initial pathname is the empty string.
+	 */
+	auto_dir();
+
+	/** Destroy auto-directory object.
+	 * Any empty directories that contain the pathname are deleted.
+	 */
+	~auto_dir();
+
+	/** Move to new pathname.
+	 * Any empty directories that contain the old pathname are deleted.
+	 * Any directories needed to reach the new pathname are created.
+	 * @param pathname the new pathname
+	 */
+	void operator()(const string& pathname);
+};
+
+}; /* namespace pkg */
+
+#endif
