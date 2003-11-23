db4abcc73d11dfbf4d628e98f0ca54aa0d183d69
gdshaw <gdshaw@40966042-f822-0410-a469-afe5762bee57>
2003-11-23 15:01:19 +0000

    Added class pkg::table.

---
+++ !LibPkg/libpkg/table.cc
@@ -0,0 +1,64 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#include "table.h"
+
+namespace pkg {
+
+table::table()
+{}
+
+table::~table()
+{
+	while (_watchers.size())
+	{
+		(*_watchers.begin())->unwatch(*this);
+	}
+}
+
+void table::register_watcher(watcher& w)
+{
+	_watchers.insert(&w);
+}
+
+void table::deregister_watcher(watcher& w)
+{
+	_watchers.erase(&w);
+}
+
+void table::notify()
+{
+	for (set<watcher*>::const_iterator i=_watchers.begin();
+		i!=_watchers.end();++i)
+	{
+		(*i)->handle_change(*this);
+	}
+}
+
+table::watcher::watcher()
+{}
+
+table::watcher::~watcher()
+{
+	for (set<table*>::const_iterator i=_tables.begin();
+		i!=_tables.end();++i)
+	{
+		(*i)->deregister_watcher(*this);
+	}
+}
+
+void table::watcher::watch(table& t)
+{
+	_tables.insert(&t);
+	t.register_watcher(*this);
+}
+
+void table::watcher::unwatch(table& t)
+{
+	_tables.erase(&t);
+	t.deregister_watcher(*this);
+}
+
+}; /* namespace pkg */
---
+++ !LibPkg/libpkg/table.h
@@ -0,0 +1,81 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#ifndef _LIBPKG_TABLE
+#define _LIBPKG_TABLE
+
+#include <set>
+
+namespace pkg {
+
+/** A base class to represent a data table.
+ * The base class does not provide any access to the content of the
+ * table.  It does implement the notification mechanism, whereby other
+ * objects can be informed when the content has changed.
+ */
+class table
+{
+public:
+	class watcher;
+	friend watcher;
+private:
+	/** The set of currently registered watchers. */
+	set<watcher*> _watchers;
+public:
+	/** Construct table. */
+	table();
+
+	/** Destroy table. */
+	~table();
+protected:
+	/** Notify watchers that a change has occurred. */
+	void notify();
+private:
+	/** Register a watcher.
+	 * This can only be done by the watcher in question (which is then
+	 * responsible for deregistering itself before it is destroyed).
+	 * @param w the watcher to be registed
+	 */
+	void register_watcher(watcher& w);
+
+	/** Deregister a watcher.
+	 * This can only be done by the watcher in question.
+	 * @param w the watcher to be deregistered
+	 */
+	void deregister_watcher(watcher& w);
+};
+
+/** A mixin class to allow an object to watch one or more tables. */
+class table::watcher
+{
+private:
+	/** The set of tables that are being watched. */
+	set<table*> _tables;
+public:
+	/** Construct watcher. */
+	watcher();
+
+	/** Destroy watcher. */
+	virtual ~watcher();
+
+	/** Begin watching table.
+	 * @param table the table to begin watching
+	 */
+	void watch(table& t);
+
+	/** Cease watching table.
+	 * @param table the table to cease watching
+	 */
+	void unwatch(table& t);
+
+	/** Handle change to table.
+	 * @param table the table that has changed
+	 */
+	virtual void handle_change(table& t)=0;
+};
+
+}; /* namespace pkg */
+
+#endif
