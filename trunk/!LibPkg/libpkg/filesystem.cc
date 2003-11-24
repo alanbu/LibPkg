e3eeca2051abc2ab0c6d27c3904123954bdd58d8
gdshaw <gdshaw@40966042-f822-0410-a469-afe5762bee57>
2003-11-24 20:54:18 +0000

    Added canonicalise(), force_delete(), force_move(), write_file_info() and object_type().

---
+++ trunk/!LibPkg/libpkg/filesystem.cc
@@ -0,0 +1,86 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#include "rtk/os/os.h"
+
+#include "libpkg/filesystem.h"
+
+namespace pkg {
+
+string canonicalise(const string& pathname)
+{
+	// Calculate required buffer size.
+	unsigned int size=0;
+	rtk::os::OS_FSControl37(pathname.c_str(),0,0,0,size,&size);
+	size=1-size;
+
+	// Canonicalise pathname.
+	char buffer[size];
+	rtk::os::OS_FSControl37(pathname.c_str(),buffer,0,0,size,&size);
+	return string(buffer);
+}
+
+void force_delete(const string& pathname)
+{
+	// Do not report an error if the file does not exist.
+	try
+	{
+		// Change file attributes to unlocked.
+		rtk::os::OS_File4(pathname.c_str(),0x33);
+
+		// Delete file.
+		rtk::os::OS_File6(pathname.c_str(),0,0,0,0,0);
+	}
+	catch (...) {}
+}
+
+void force_move(const string& src_pathname,const string& dst_pathname,
+	bool overwrite)
+{
+	if (overwrite)
+	{
+		// If overwrite flag set then attempt to delete destination,
+		// but do not report an error if it does not exist.
+		try
+		{
+			// Change file attributes to unlocked.
+			rtk::os::OS_File4(dst_pathname.c_str(),0x33);
+
+			// Delete file.
+			rtk::os::OS_File6(dst_pathname.c_str(),0,0,0,0,0);
+		}
+		catch (...) {}
+	}
+
+	// Read source file attributes.
+	unsigned int attr;
+	rtk::os::OS_File17(src_pathname.c_str(),0,0,0,0,&attr);
+
+	// Change source file attributes to unlocked (if necessary).
+	if (attr&0x08) rtk::os::OS_File4(src_pathname.c_str(),attr&~0x08);
+
+	// Move file.
+	rtk::os::OS_FSControl25(src_pathname.c_str(),dst_pathname.c_str());
+
+	// Change destination file attributes to locked (if appropriate).
+	if (attr&0x08) rtk::os::OS_File4(dst_pathname.c_str(),attr);
+}
+
+void write_file_info(const string& pathname,unsigned int loadaddr,
+	unsigned int execaddr,unsigned int attr)
+{
+	// Write load address, execution address and file attributes.
+	rtk::os::OS_File1(pathname.c_str(),loadaddr,execaddr,attr);
+}
+
+unsigned int object_type(const string& pathname)
+{
+	// Read object type.
+	unsigned int objtype;
+	rtk::os::OS_File17(pathname.c_str(),&objtype,0,0,0,0);
+	return objtype;
+}
+
+}; /* namespace pkg */
---
+++ trunk/!LibPkg/libpkg/filesystem.h
@@ -0,0 +1,64 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#ifndef _LIBPKG_FILESYSTEM
+#define _LIBPKG_FILESYSTEM
+
+#include <string>
+
+namespace pkg {
+
+/** Canonicalise pathname.
+ * @param pathname the pathname to be canonicalised
+ * @return the canonicalised pathname
+ */
+string canonicalise(const string& pathname);
+
+/** Delete file without regard for file attributes.
+ * If the file attributes prevent deletion then they are changed.
+ * It is not an error if the file does not exist.
+ * It is an error if the file cannot be deleted (after attempting
+ * to change the file attributes).
+ * @param pathname the pathname of the file to be deleted
+ */
+void force_delete(const string& pathname);
+
+/** Move file without regard for file attributes.
+ * If the file attributes prevent movement then they are changed for the
+ * duration of the operation then restored afterwards.
+ * If the overwrite flag is set then an existng file at the destination
+ * will be overwritten, also without regard for its file attributes.
+ * It is an error if the source file does not exist, or cannot be moved
+ * (after changing its attributes if necessary).
+ * It is not an error if overwrite is requested but the destination does
+ * not exist.
+ * The source and destination must be on the same disc.
+ * @param src_pathname the pathname of the file to be moved.
+ * @param dst_pathname the pathname the file is to be moved to.
+ * @param overwrite true if overwriting permitted, otherwise false.
+ */
+void force_move(const string& src_pathname,const string& dst_pathname,
+	bool overwrite=false);
+
+/** Write file information.
+ * A filetype and timestamp may be given in place of a load address and
+ * execution address by encoding them in the normal way.
+ * @param pathname the pathname of the file
+ * @param loadaddr the required load address
+ * @param execaddr the required execution address
+ * @param attr the required file attributes
+ */
+void write_file_info(const string& pathname,unsigned int loadaddr,
+	unsigned int execaddr,unsigned int attr);
+
+/** Get object type.
+ * @param pathname the pathname
+ * @return the object type
+ */
+unsigned int object_type(const string& pathname);
+
+}; /* namespace pkg */
+
+#endif
