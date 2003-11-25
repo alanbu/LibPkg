03bdc37cfd6b5c0eff2526b17aad7a85e0670dde
gdshaw <gdshaw@40966042-f822-0410-a469-afe5762bee57>
2003-11-25 19:20:00 +0000

    Added class pkg::download.

---
+++ !LibPkg/libpkg/download.h
@@ -0,0 +1,111 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#ifndef _LIBPKG_DOWNLOAD
+#define _LIBPKG_DOWNLOAD
+
+#include <string>
+#include <fstream>
+
+#include "curl/curl.h"
+
+namespace pkg {
+
+/** A class for downloading a file from a URL. */
+class download
+{
+public:
+	/** A type for representing byte counts. */
+	typedef unsigned long long size_type;
+
+	/** A null value for use in place of a byte count. */
+	static const size_type npos=static_cast<size_type>(-1);
+private:
+	/** The libcurl easy handle. */
+	CURL* _ceasy;
+
+	/** The URL from which to download. */
+	string _url;
+
+	/** The stream to which the file is to be written. */
+	ofstream _out;
+
+	/** The done flag.
+	 * True if the download has finished, otherwise false.
+	 */
+	bool _done;
+
+	/** The number of bytes downloaded. */
+	size_type _bytes_done;
+
+	/** The total number of bytes to download, or npos if not known. */
+	size_type _bytes_total;
+public:
+	/** Construct download action.
+	 * @param url the URL from which to download
+	 * @param pathname the pathname to which the file is to be written
+	 */
+	download(const string& url,const string& pathname);
+
+	/** Destroy download action. */
+	~download();
+
+	/** Get done flag.
+	 * @return true if download has finished, otherwise false.
+	 */
+	bool done()
+		{ return _done; }
+
+	/** Get number of bytes downloaded.
+	 * @return the number of bytes downloaded
+	 */
+	size_type bytes_done()
+		{ return _bytes_done; }
+
+	/** Get total number of bytes to download.
+	 * @return the total number of bytes, or npos if not known
+	 */
+	size_type bytes_total()
+		{ return _bytes_total; }
+
+	/** Handler for CURLOPT_WRITEFUNCTION callbacks.
+	 * @param buffer the data buffer
+	 * @param size the size of each data item
+	 * @param nitems the number of data items
+	 * @return the number of bytes written (size*nitems)
+	 */
+	size_t write_callback(char* buffer,size_t size,size_t nitems);
+
+	/** Handler for CURLOPT_PROGRESSFUNCTION callbacks.
+	 * @param dltotal the total number of bytes to download, or 0 if not known
+	 * @param dlnow the number of bytes downloaded
+	 * @return zero
+	 */
+	int progress_callback(double dltotal,double dlnow);
+
+	/** Handler for Curl messages.
+	 * @param msg the message
+	 */
+	void message_callback(CURLMsg* msg);
+private:
+	/** The libcurl multi handle.
+	 * This is shared between all downloads.  A multi handle is created
+	 * whenever there is at least one easy handle to attach to it.  When
+	 * all easy handles have been removed, the multi handle is deleted
+	 * (and the pointer set to null).
+	 */
+	static CURLM* _cmulti;
+
+	/** The number of libcurl easy handles attached to the shared
+	 * multi handle. */
+	static unsigned int _cmulti_refcount;
+public:
+	/** Poll all download operations. */
+	static void poll_all();
+};
+
+}; /* namespace pkg */
+
+#endif
