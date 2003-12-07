f560aa431ece54fbf470f7e030d2753b1828409e
gdshaw <gdshaw@40966042-f822-0410-a469-afe5762bee57>
2003-12-07 09:26:42 +0000

    Added pkg::can_process().

---
+++ !LibPkg/libpkg/standards_version.cc
@@ -0,0 +1,23 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#include "libpkg/version.h"
+#include "libpkg/standards_version.h"
+
+namespace pkg {
+
+namespace {
+
+/** The earliest standards version that cannot be processed by this library. */
+const version bad_standards_version("0.1");
+
+}; /* anonymous namespace */
+
+bool can_process(const version& standards_version)
+{
+	return standards_version<bad_standards_version;
+}
+                 
+}; /* namespace pkg */
