c97586adcbcd92a28399c6773113f4722a463505
nobody <nobody@40966042-f822-0410-a469-afe5762bee57>
2003-12-24 10:59:01 +0000

    This commit was manufactured by cvs2svn to create tag 'v0_0_0'.

---
+++ tags/v0_0_0/!LibPkg/libpkg/pkgbase.h
@@ -0,0 +1,225 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#ifndef _LIBPKG_PKGBASE
+#define _LIBPKG_PKGBASE
+
+#include <string>
+
+#include "libpkg/dependency.h"
+#include "libpkg/status_table.h"
+#include "libpkg/binary_control_table.h"
+#include "libpkg/source_table.h"
+#include "libpkg/path_table.h"
+
+namespace pkg {
+
+/** A class for representing the collection of package database tables. */
+class pkgbase
+{
+private:
+	/** The pathname of the !Packages directory. */
+	string _pathname;
+
+	/** The pathname of the default choices directory. */
+	string _dpathname;
+
+	/** The pathname of the choices directory. */
+	string _cpathname;
+
+	/** The current status table. */
+	status_table _curstat;
+
+	/** The selected status table. */
+	status_table _selstat;
+
+	/** The previous status table. */
+	status_table _prevstat;
+
+	/** The binary control table. */
+	binary_control_table _control;
+
+	/** The source table. */
+	source_table _sources;
+
+	/** The path table. */
+	path_table _paths;
+
+	/** The changed flag.
+	 * True if any of the must-remove, must-install or must-upgrade
+	 * flags have changed since the start of the current round of
+	 * dependency resolution.  Note that this flag is only meaningful
+	 * during dependency resolution, and it only captures changes
+	 * made using the functions ensure_removed() and ensure_installed().
+	 */
+	bool _changed;
+public:
+	/** Create pkgbase object.
+	 * @param pathname the pathname of the !Packages directory.
+	 * @param dpathname the pathname of the default choices directory
+	 * @param cpathname the pathname of the choices directory
+	 */
+	pkgbase(const string& pathname,const string& dpathname,
+		const string& cpathname);
+
+	/** Destroy pkgbase object. */
+	~pkgbase();
+
+	/** Get current status table.
+	 * @return the current status table
+	 */
+	status_table& curstat()
+		{ return _curstat; }
+
+	/** Get selected status table.
+	 * @return the selected status table
+	 */
+	status_table& selstat()
+		{ return _selstat; }
+
+	/** Get previous status table.
+	 * This is the state to which packages are restored when
+	 * changes are unwound after an error.
+	 * @return the previous status table
+	 */
+	status_table& prevstat()
+		{ return _prevstat; }
+
+	/** Get binary control table.
+	 * @return the binary control table
+	 */
+	binary_control_table& control()
+		{ return _control; }
+
+	/** Get source table.
+	 * @return the source table
+	 */
+	source_table& sources()
+		{ return _sources; }
+
+	/** Get path table.
+	 * @return the path table
+	 */
+	path_table& paths()
+		{ return _paths; }
+
+	/** Get pathname for index file from given source.
+	 * @param url the URL of the source
+	 * @return the pathname
+	 */
+	string list_pathname(const string& url);
+
+	/** Get pathname for available list file.
+	 * @return the pathname
+	 */
+	string available_pathname();
+
+	/** Get pathname for package in cache.
+	 * @param pkgname the package name
+	 * @param pkgvrsn the package version
+	 * @return the pathname
+	 */
+	string cache_pathname(const string& pkgname,
+		const string& pkgvrsn);
+
+	/** Get pathname for info directory of package.
+	 * @param pkgname the package name
+	 * @return the pathname
+	 */
+	string info_pathname(const string& pkgname);
+
+	/** Get pathname for sysvars directory.
+	 * @return the pathname
+	 */
+	string sysvars_pathname();
+
+	/** Get pathname for setvars file.
+	 * @return the pathname
+	 */
+	string setvars_pathname(); 
+
+	/** Fix dependencies.
+	 * If a package is in the seed set then its selection state cannot
+	 * change from installed to removed or vice-versa.  If it is not in
+	 * the seed set then it can be installed or removed as necessary to
+	 * meet all dependencies.
+	 * @param seed the seed set
+	 * @return true if all dependencies were fixed, otherwise false
+	 */
+	bool fix_dependencies(const set<string>& seed);
+
+	/** Remove redundant auto-installed packages.
+	 */
+	void remove_auto();
+private:
+	/** Fix dependencies for package.
+	 * Flags are altered if and only if all dependencies can be satisifed.
+	 * @param ctrl the package control record
+	 * @param allow_new true to allow packages that are not currently
+	 *  installed, otherwise false
+	 * @param apply true to apply changes to must-remove, must-install
+	 *  and must-upgrade flags, otherwise false
+	 * @return true if all dependencies were satisfied, otherwise false
+	 */
+	bool fix_dependencies(const pkg::control& ctrl,bool allow_new);
+
+	/** Fix dependencies for package.
+	 * @param ctrl the package control record
+	 * @param allow_new true to allow packages that are not currently
+	 *  installed, otherwise false
+	 * @param apply true to apply changes to must-remove, must-install
+	 *  and must-upgrade flags, otherwise false
+	 * @return true if all dependencies were satisfied, otherwise false
+	 */
+	bool fix_dependencies(const pkg::control& ctrl,bool allow_new,
+		bool apply);
+
+	/** Resolve dependency alternatives.
+	 * Preference is given to packages that are already installed
+	 * (and are not flagged for removal), followed by packages that
+	 * are flagged for installation.
+	 * If two or more equally good solutions are available, the one
+	 * chosen is the one that occurs earliest in the list.
+	 * @param deps the list of dependencies
+	 * @param allow_new true to allow packages that are not currently
+	 *  installed, otherwise false
+	 * @return the control record of a package that would satisfy one
+	 *  of the listed dependencies, or 0 if none found
+	 */
+	const pkg::control* pkgbase::resolve(const vector<dependency>& deps,
+		bool allow_new=true);
+
+	/** Resolve dependency.
+	 * Preference is given to packages that are already installed
+	 * (and are not flagged for removal), followed by packages that
+	 * are flagged for installation.
+	 * @param dep the depenency to be satisfied
+	 * @param allow_new true to allow packages that are not currently
+	 *  installed, otherwise false
+	 * @return the control record of a package that would satisfy the
+	 *  dependency, or 0 if none found
+	 */
+	const pkg::control* pkgbase::resolve(const dependency& dep,
+		bool allow_new=true);
+
+	/** Ensure that package will be removed.
+	 * The must-remove flag is set if it is not already.
+	 * @param pkgname the package name
+	 * @param pkgvrsn the package version
+	 */
+	void ensure_removed(const string& pkgname);
+
+	/** Ensure that package will be installed.
+	 * The must-install flag, and if necessary the must-upgrade flag,
+	 * are set if they are not already.
+	 * @param pkgname the package name
+	 * @param pkgvrsn the package version
+	 */
+	void ensure_installed(const string& pkgname,const string& pkgvrsn);
+};
+
+}; /* namespace pkg */
+
+#endif
---
+++ tags/v0_0_0/!LibPkg/libpkg/update.cc
@@ -0,0 +1,237 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#include "libpkg/filesystem.h"
+#include "libpkg/version.h"
+#include "libpkg/status.h"
+#include "libpkg/binary_control.h"
+#include "libpkg/pkgbase.h"
+#include "libpkg/download.h"
+#include "libpkg/update.h"
+
+namespace pkg {
+
+update::update(pkgbase& pb):
+	_pb(pb),
+	_state(state_srclist),
+	_dload(0),
+	_out(0),
+	_bytes_done(0),
+	_bytes_total(npos)
+{}
+
+update::~update()
+{}
+
+void update::poll()
+{
+	try
+	{
+		_poll();
+	}
+	catch (exception& ex)
+	{
+		_message=ex.what();
+		delete _dload;
+		_dload=0;
+		delete _out;
+		_out=0;
+		_state=state_fail;
+	}
+}
+
+void update::_poll()
+{
+	switch (_state)
+	{
+	case state_srclist:
+		// Re-read sources list from disc.
+		_pb.sources().update();
+
+		// For each source, create an entry in the progress table, and
+		// measure the length of the most recent download if there was one.
+		for (source_table::const_iterator i=_pb.sources().begin();
+			i!=_pb.sources().end();++i)
+		{
+			_url=*i;
+			_progress_table[_url];
+			string pathname=_pb.list_pathname(_url);
+			unsigned int objtype=object_type(pathname);
+			if (objtype)
+				_progress_table[_url].bytes_prev=object_length(pathname);
+			_sources_to_download.insert(_url);
+		}
+
+		// Switch to state_download.
+		_state=state_download;
+		break;
+	case state_download:
+		if (_dload)
+		{
+			// Monitor download in progress.
+			update_progress();
+			switch (_dload->state())
+			{
+			case download::state_download:
+				// If download in progress then do nothing.
+				break;
+			case download::state_done:
+				// If download complete then move to next source.
+				delete _dload;
+				_dload=0;
+				_sources_to_build.insert(_url);
+				_sources_to_download.erase(_url);
+				break;
+			case download::state_fail:
+				// If download failed then update failed too.
+				_message=_dload->message();
+				delete _dload;
+				_dload=0;
+				_state=state_fail;
+				break;
+			}
+		}
+		else if (_sources_to_download.size())
+		{
+			// If there are sources awaiting download then begin
+			// downloading the first.
+			_url=*_sources_to_download.begin();
+			string pathname=_pb.list_pathname(_url);
+			_dload=new download(_url,pathname);
+		}
+		else
+		{
+			// If there are no sources awaiting download then
+			// switch to state_build_sources.
+			_out=new ofstream(_pb.available_pathname().c_str());
+			_state=state_build_sources;
+		}
+		break;
+	case state_build_sources:
+		if (_sources_to_build.size())
+		{
+			// Select next source.
+			_url=*_sources_to_build.begin();
+			string pathname=_pb.list_pathname(_url);
+			ifstream in(pathname.c_str());
+
+			while (in&&!in.eof())
+			{
+				// Read control record from source.
+				binary_control ctrl;
+				in >> ctrl;
+
+				// Extract package name and version.
+				string pkgname=ctrl.pkgname();
+				version pkgvrsn=ctrl.version();
+				binary_control_table::key_type key(pkgname,pkgvrsn);
+				if (_packages_written.find(key)==_packages_written.end())
+				{
+					// If not already written then write to available list.
+					if (_packages_written.size()) (*_out) << endl;
+					(*_out) << ctrl;
+					_packages_written.insert(key);
+				}
+
+				// Absorb newlines between packages.
+				while (in.peek()=='\n') in.get();
+			}
+			_sources_to_build.erase(_url);
+		}
+		else
+		{
+			// If there are no more sources waiting to be built
+			// then switch to state_build_local.
+			_state=state_build_local;
+		}
+		break;
+	case state_build_local:
+		{
+			status_table& curstat=_pb.curstat();
+			for (status_table::const_iterator i=curstat.begin();
+				i!=curstat.end();++i)
+			{
+				string pkgname=i->first;
+				string pathname=_pb.info_pathname(pkgname)+string(".Control");
+				ifstream in(pathname.c_str());
+				binary_control ctrl;
+				in >> ctrl;
+
+				// Extract package version.
+				version pkgvrsn=ctrl.version();
+				binary_control_table::key_type key(pkgname,pkgvrsn);
+				if (_packages_written.find(key)==_packages_written.end())
+				{
+					// If not already written then write to available list.
+					if (_packages_written.size()) (*_out) << endl;
+					(*_out) << ctrl;
+					_packages_written.insert(key);
+				}
+			}
+
+			// Close output stream, update package database
+			// from available file, and switch to state_done.
+			delete _out;
+			_out=0;
+			_pb.control().update();
+			_state=state_done;
+		}
+		break;
+	case state_done:
+		break;
+	case state_fail:
+		break;
+	}
+}
+
+void update::update_progress()
+{
+	// If download active then update progress for that source.
+	if (_dload)
+	{
+		progress& pr=_progress_table[_url];
+		pr.bytes_done=_dload->bytes_done();
+		pr.bytes_total=_dload->bytes_total();
+	}
+
+	// Sum progress over all sources.  Calculate number of sources (count)
+	// and number for which a total or estimated total is available (known).
+	_bytes_done=0;
+	_bytes_total=0;
+	unsigned int count=0;
+	unsigned int known=0;
+	for (map<string,progress>::const_iterator i=_progress_table.begin();
+		i!=_progress_table.end();++i)
+	{
+		_bytes_done+=i->second.bytes_done;
+		if (i->second.bytes_total!=npos)
+		{
+			// If total is available for this source then use it.
+			_bytes_total+=i->second.bytes_total;
+			++known;
+		}
+		else if (i->second.bytes_prev!=npos)
+		{
+			// Otherwise, if a total is available from a previous download
+			// then use that instead.
+			_bytes_total+=i->second.bytes_prev;
+			++known;
+		}
+		++count;
+	}
+
+	// Use linear extrapolation to estimate sources for which no
+	// information is available (provided there is at least one total
+	// or estimated total from which to extrapolate).
+	if (known) _bytes_total+=(_bytes_total*(count-known))/known;
+}
+
+update::progress::progress():
+	bytes_done(0),
+	bytes_total(npos),
+	bytes_prev(npos)
+{}
+
+}; /* namespace pkg */
