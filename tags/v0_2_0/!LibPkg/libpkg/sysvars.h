0d402c33ee39a93ea80e3b6b1338c373408b3fd5
nobody <nobody@40966042-f822-0410-a469-afe5762bee57>
2004-03-13 09:24:50 +0000

    This commit was manufactured by cvs2svn to create tag 'v0_2_0'.

---
+++ tags/v0_2_0/!LibPkg/libpkg/sysvars.h
@@ -0,0 +1,22 @@
+// This file is part of LibPkg.
+// Copyright © 2003 Graham Shaw.            
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#ifndef _LIBPKG_SYSVARS
+#define _LIBPKG_SYSVARS
+
+namespace pkg {
+
+class pkgbase;
+
+/** Update system variable definitions.
+ * System variable definitions are found and merged into a single obey
+ * file which is executed at boot time.
+ * @param pb the package database
+ */
+void update_sysvars(pkgbase& pb);
+
+}; /* namespace pkg */
+
+#endif
