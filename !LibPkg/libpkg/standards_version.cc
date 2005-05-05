fab2a689f2e17ccc5391e6886f1ba6b21b7bf6b0
gdshaw <gdshaw@40966042-f822-0410-a469-afe5762bee57>
2005-05-05 20:28:40 +0000

    Removal of unwanted whitespace.

--- !LibPkg/libpkg/standards_version.cc
+++ !LibPkg/libpkg/standards_version.cc
@@ -1,5 +1,5 @@
 // This file is part of LibPkg.
-// Copyright © 2003-2004 Graham Shaw.            
+// Copyright © 2003-2004 Graham Shaw.
 // Distribution and use are subject to the GNU Lesser General Public License,
 // a copy of which may be found in the file !LibPkg.Copyright.
 
@@ -19,5 +19,5 @@ bool can_process(const version& standards_version)
 {
 	return standards_version<bad_standards_version;
 }
-                 
+
 }; /* namespace pkg */
