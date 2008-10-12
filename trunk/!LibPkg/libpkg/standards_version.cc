15050b80bc222770c34a7231541e575f8b97c4b9
gdshaw@RISCID.ORG <gdshaw@RISCID.ORG@40966042-f822-0410-a469-afe5762bee57>
2008-10-12 13:03:55 +0000

    Raised supported standards version from 0.1.* to 0.2.*.

--- trunk/!LibPkg/libpkg/standards_version.cc
+++ trunk/!LibPkg/libpkg/standards_version.cc
@@ -11,7 +11,7 @@ namespace pkg {
 namespace {
 
 /** The earliest standards version that cannot be processed by this library. */
-const version bad_standards_version("0.2");
+const version bad_standards_version("0.3");
 
 }; /* anonymous namespace */
 
