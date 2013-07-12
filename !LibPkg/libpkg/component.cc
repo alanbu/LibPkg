f46575fbd3556ff05f55951d169ab3ffdd14617e
alanb <alanbu@users.noreply.github.com>
2013-07-12 07:43:29 +0000

    Added component class as first step towards adding components to packages

---
+++ !LibPkg/libpkg/component.cc
@@ -0,0 +1,210 @@
+// This file is part of LibPkg.
+// Copyright © 2003-2005 Graham Shaw.
+// Copyright © 2013 Alan Buckley.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#include "libpkg/component.h"
+#include <cctype>
+
+namespace pkg
+{
+
+const char*ComponentFlagNames[] = 
+{
+	"Moveable",
+	"LookAt",
+	"Run",
+	"AddToApps"
+};
+	
+component::component() : _flags(0) {}
+
+component::component(std::string::const_iterator first, std::string::const_iterator last)
+{
+	parse(first, last);
+}
+
+component::component(const std::string& compstr)
+{
+	parse(compstr.begin(), compstr.end());
+}
+
+component::~component()
+{
+}
+
+bool component::operator==(const component &other) const
+{
+	return (_name == other._name && _flags == other._flags && _path == other._path);
+}
+
+bool component::operator!=(const component &other) const
+{
+	return (_name != other._name && _flags != other._flags && _path != other._path);
+}
+
+
+component::operator std::string() const
+{
+	std::string out(_name);
+	if (_flags)
+	{
+		out += " (";
+		bool add_space = false;
+		for (int f = (int)moveable; f <= (int)add_to_apps; ++f)
+		{
+			if (add_space) out += " ";
+			else add_space = true;
+			if (flag((flag_type)f)) out += ComponentFlagNames[f];
+		}
+		out += ")";
+	}
+
+	return out;
+}
+
+void component::flag(flag_type flag,bool value)
+{
+	if (value) _flags |= (1<<flag);
+	else _flags &= ~(1<<flag);
+}
+
+void component::parse(std::string::const_iterator first,std::string::const_iterator last)
+{
+	// Initialise iterator.
+	std::string::const_iterator p=first;
+	_flags = 0;
+
+	// Parse package name.
+	std::string::const_iterator q=p;
+	while ((p!=last)&&(*p!=' ')&&(*p!='('))
+	{
+		char ch=*p++;
+		if (!std::isalnum(ch)&&(ch!='+')&&(ch!='-')&&(ch!='.'))
+			throw parse_error("illegal character in component name");
+	}
+	if (p==q) throw parse_error("component name expected");
+	_name=std::string(q,p);
+
+	// If not at end of sequence, parse flags.
+	if (p!=last)
+	{
+		// Skip whitespace.
+		while ((p!=last)&&(*p==' ')) ++p;
+
+		// Parse '('.
+		if ((p!=last)&&(*p=='(')) ++p;
+		else throw parse_error("'(' or end of component expected");
+
+		// Skip whitespace.
+		while ((p!=last)&&(*p==' ')) ++p;
+
+		// Loop through flags
+		while ((p!=last)&& *p!=')')
+		{
+			std::string::const_iterator flag_start = p;
+			while ((p!=last)&&(*p!=' ')&&(*p!=')')) ++p;
+			std::string flag_name(flag_start, p);
+			unsigned int old_flags = _flags;
+			for(int f = (int)moveable; f <= (int)add_to_apps; ++f)
+			{
+				if (flag_name == ComponentFlagNames[f]) flag((flag_type)f, true);
+			}
+			if (old_flags == _flags) parse_error(std::string("invalid component flag '" + flag_name + "'").c_str());
+
+			// Skip whitespace.
+			while ((p!=last)&&(*p==' ')) ++p;
+		}
+
+		if (_flags)
+		{
+			if ((p==last)||(*p!=')')) throw parse_error("')' missing from end of component flags");
+			p++;
+		}
+	}
+
+	// There should now be no characters remaining.
+	if (p!=last) throw parse_error("end of component expected");
+}
+
+component::parse_error::parse_error(const char* message):
+	std::runtime_error(message)
+{}
+
+void parse_component_list(std::string::const_iterator first,
+	std::string::const_iterator last,std::vector<component>*out)
+{
+	// Initialise iterator.
+	std::string::const_iterator p=first;
+
+	// Repeat until end of sequence.
+	while (p!=last)
+	{
+		// Mark start of sub-list.
+		std::string::const_iterator q=p;
+
+		// Search for list delimeter (comma).
+		while ((p!=last)&&(*p!=',')) ++p;
+
+		// Mark end of sub-list.
+		std::string::const_iterator r=p;
+
+		// If delimeter found, strip whitespace from end of sub-list.
+		if (p!=last)
+		{
+			while ((r!=q)&&(*(r-1)==' ')) --r;
+		}
+
+		// Parse sub-list.
+		if (r==q) throw component::parse_error("component expected");
+		component comp(q,r);
+		if (out) out->push_back(comp);
+
+		// If delimeter found, skip it plus any whitespace, but a
+		// dependency must follow.
+		if (p!=last)
+		{
+			++p;
+			while ((p!=last)&&(*p==' ')) ++p;
+			if (p==last) throw component::parse_error("component expected");
+		}
+	}
+}
+
+std::ostream& operator<<(std::ostream& out,
+	const component &comp)
+{
+	out << ((std::string)comp) << "\t";
+	out << comp.path() << std::endl;
+
+	return out;
+}
+
+/** Read component record from input stream.
+ * @param in the input stream
+ * @param comp the component record
+ * @return the input stream
+ */
+std::istream& operator>>(std::istream& in, component &comp)
+{
+	// Read line from input stream.
+	std::string line;
+	std::getline(in,line);
+	std::string::size_type tab_pos = line.find('\t');
+	if (tab_pos == std::string::npos)
+	{
+		comp = component(line.begin(), line.end());
+	} else
+	{
+		std::string::iterator tab_iter = line.begin() + tab_pos;
+		comp = component(line.begin(), tab_iter);
+		if (++tab_iter == line.end()) comp.path("");
+		else comp.path(std::string(tab_iter, line.end()));
+	}
+
+	return in;
+}
+
+
+}; /* namespace pkg */
---
+++ !LibPkg/libpkg/component.h
@@ -0,0 +1,169 @@
+// This file is part of LibPkg.
+// Copyright © 2003-2005 Graham Shaw.
+// Copyright © 2013 Alan Buckley.
+// Distribution and use are subject to the GNU Lesser General Public License,
+// a copy of which may be found in the file !LibPkg.Copyright.
+
+#ifndef LIBPKG_COMPONENT
+#define LIBPKG_COMPONENT
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+namespace pkg
+{
+	
+/**
+ * A class to represent a component of a package
+ *
+ * A component is a file or folder that has extra metadata associated with it
+ * to configure it at install time.
+ *
+ * From LibPkg 0.4 any path that can be set at install time should
+ * have a component included with the Moveable flag set.
+ */
+class component
+{
+public:
+	/** The options available/selected for this component
+	 */
+	enum flag_type
+	{
+		/** A flag to indicate this component can have it's install location
+		 *  changed and it may be moved after install. */
+		moveable,
+		/** A flag to indicate this component should be added to the RISC OS
+		 *  boot look at file, so it is booted when the desktop starts. */
+		look_at,
+		/** A flag to indicate this component should be added to the RISC OS
+		 *  boot run file, so it is run when the desktop starts. */
+		run,
+		/** A flag to indicate this component should be added to the RISC OS
+		 *  application pseudo folder. */
+		add_to_apps
+	};
+
+	class parse_error;
+private:
+	std::string _name;
+	unsigned int _flags;
+	std::string _path;
+
+public:
+	/**
+	 * Empty constructor, unnamed component with no flags
+	 */
+	component();
+
+	/**
+	 * Construct a component from a sequence.
+	 * 
+	 * This is the name followed by space separated options in brackets
+	 *
+	 * @param first beginning of sequence
+	 * @param last end of sequence
+	 */
+	component(std::string::const_iterator first, std::string::const_iterator last);
+
+	/** Construct component from string.
+	 * @param compstr the component string
+	 */
+	component(const std::string& compstr);
+
+	/** Destroy component. */
+	~component();
+
+	/** Check if it is the same as another component */
+	bool operator==(const component &other) const;
+	/** Check if it is different from another component */
+	bool operator!=(const component &other) const;
+
+	/** Convert component to string.
+	 * Does not include the path
+	 * @return the component as a string
+	 */ 
+	operator std::string() const;
+
+	/** Get the name of the component
+	 * The name is the same as the logical path to the component.
+	 * @returns the component name
+	 */
+	const std::string &name() const {return _name;}
+
+	/**
+	 * Get the bit field containing the component flags
+	 */
+	unsigned int flags() const {return _flags;}
+
+	/** Get component flag.
+	 * @param flag the flag to be read
+	 * @return the value of the flag
+	 */
+	bool flag(flag_type flag) const
+		{ return (_flags>>flag)&1; }
+
+	/** Set component flag.
+	 * @param flag the flag to be altered
+	 * @param value the required value
+	 */
+	void flag(flag_type flag,bool value);
+
+	/** Get the installation path
+	 * @returns path or empty string for the default path.
+	 */
+	const std::string &path() const {return _path;}
+	/**
+	 * Set the installation path
+	 * @param path the new installation path for the component
+	 */
+	void path(const std::string &path) {_path = path;}
+
+private:
+	/** Parse component.
+	 * @param first the beginning of the sequence
+	 * @param last tne end of the sequence
+	 */
+	void parse(std::string::const_iterator first,std::string::const_iterator last);
+
+};
+
+/** An exception class for reporting parse errors. */
+class component::parse_error:
+	public std::runtime_error
+{
+public:
+	/** Construct parse error.
+	 * @param message a message which describes the parse error.
+	 */
+	parse_error(const char* message);
+};
+
+/** Parse component list.
+ * @param first the beginning of the sequence
+ * @param last the end of the sequence
+ * @param out a vector to which the coponents are added
+ */
+void parse_component_list(std::string::const_iterator first,
+	std::string::const_iterator last,std::vector<component> *out);
+
+/** Write component record to output stream.
+ * @param out the output stream
+ * @param comp the component record
+ * @return the output stream
+ */
+std::ostream& operator<<(std::ostream& out,
+	const component &comp);
+
+/** Read component record from input stream.
+ * @param in the input stream
+ * @param comp the component record
+ * @return the input stream
+ */
+std::istream& operator>>(std::istream& in, component &comp);
+
+
+}; /* namespace pkg */
+
+#endif
